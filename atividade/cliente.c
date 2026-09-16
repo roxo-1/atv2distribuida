83931ecf6ac7e779e15c43e474181d410f0c16ce
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2026-09-16 13:31:49 +0000

    nada de relevante, não entendi nada

---
+++ atividade/cliente.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORTA         9090
+#define SERVIDOR_IP   "127.0.0.1"
+/*
+cria socket
+conecta os dois jogadores?
+recebe nome
+sla
+*/
---
+++ atividade/servidor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORTA 9090
